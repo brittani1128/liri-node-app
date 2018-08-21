a77b32fce8d262f855b671b71251934daa05e9fd
Brittani <brittani1128@gmail.com>
2018-08-20 23:12:44 -0500

    everything working but dowhatitsays

--- liri.js
+++ liri.js
@@ -1,7 +1,7 @@
 //TO DO
 
 //convert event date in getArtist
-//dowhatitsays
+//dowhatitsays -- fix bug
 
 //log.txt
 //readme
@@ -16,13 +16,14 @@ var fs = require("fs");
 var moment = require("moment");
 var chalk = require("chalk");
 
+
 var command = process.argv[2];
 switch (command) {
     case "movie-this":
         getMovie();
         break;
     case "concert-this":
-        getArtist();
+        concertThis();
         break;
     case "spotify-this-song":
         getSong();
@@ -32,28 +33,40 @@ switch (command) {
         break;
 }
 
+
 //OMDB ====================================================
 
-var movieTitle;
+var movie;
 function getMovie() {
-    movieTitle = process.argv[3];
-    if (!movieTitle){
-        console.log(chalk.red("ERROR: You did not provide a movie. How about: Mr. Nobody"));
-        movieTitle = "mr+nobody"
+
+    //Grab user input
+    movie = process.argv[3];
+    for (var i = 4; i < process.argv.length; i++) {
+        movie += (" " + process.argv[i]);
     }
-    var queryUrl = "http://www.omdbapi.com/?t=" + movieTitle + "&y=&plot=short&apikey=" + keys.omdb;
+    
+    //If user doesn't enter movie, return error
+    if (!movie){
+        console.log(chalk.red("ERROR: You did not provide a movie. How about: Mr. Nobody"));
+        movie = "mr+nobody"
+    } else {
+        movie = movie.trim().replace(" ", "+");
+    }
+
+    //Search OMDB for movie
+    var queryUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=" + keys.omdb;
     request(queryUrl, function (error, response, body) {
         if (!error && response.statusCode === 200) {
-            var body = JSON.parse(body);
+            var data = JSON.parse(body);
             console.log("------------------------------------");
-            console.log(chalk.bold("Movie title: ")              + body.Title +
-                        chalk.bold("\nReleased: " )              + body.Year +
-                        chalk.bold("\nIMDB Rating: ")            + body.Ratings[0].Value +
-                        chalk.bold("\nRotten Tomatoes Rating: ") + body.Ratings[1].Value +
-                        chalk.bold("\nProduced in: ")            + body.Country +
-                        chalk.bold("\nLanguage(s): ")            + body.Language +
-                        chalk.bold("\nPlot: ")                   + body.Plot +
-                        chalk.bold("\nActors: ")                 + body.Actors);
+            console.log(chalk.bold("Movie title: ")              + data.Title +
+                        chalk.bold("\nReleased: " )              + data.Year +
+                        chalk.bold("\nIMDB Rating: ")            + data.Ratings[0].Value +
+                        chalk.bold("\nRotten Tomatoes Rating: ") + data.Ratings[1].Value +
+                        chalk.bold("\nProduced in: ")            + data.Country +
+                        chalk.bold("\nLanguage(s): ")            + data.Language +
+                        chalk.bold("\nPlot: ")                   + data.Plot +
+                        chalk.bold("\nActors: ")                 + data.Actors);
             console.log("------------------------------------");
         }
     })
@@ -61,22 +74,33 @@ function getMovie() {
 
 
 //BandsInTown =============================================
-
 var artist;
-function getArtist() {
+function concertThis() {
+
+    //Grab user input
     artist = process.argv[3];
+    for (var i = 4; i < process.argv.length; i++) {
+        artist += (" " + process.argv[i]);
+    }
+
+    //If user doesn't enter artist, return error
     if (!artist){
         console.log(chalk.red("ERROR: You did not provide an artist!"));
         return;
+    } else {
+        artist = artist.trim();
     }
 
+    //Search bandsintown for artist
     var queryUrl =
         "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=" + keys.bandsintown;
     request(queryUrl, function (error, response, body) {
         if (!error && response.statusCode === 200) {
             var body = JSON.parse(body);
+            
             console.log("---------------------------------------" +
-                        chalk.bold("\nVenue Name: ") + body[0].venue.name + 
+                        chalk.bold.blue("\nNEXT SHOW FOR " + artist.toUpperCase()) +
+                        chalk.bold("\n\nVenue Name: ") + body[0].venue.name + 
                         chalk.bold("\nVenue Location: ") + body[0].venue.city + ", " + body[0].venue.country +
                         chalk.bold("\nDate: ") +
                         "\n---------------------------------------");
@@ -87,21 +111,30 @@ function getArtist() {
     });
 }
 
-//Spotify This Song ========================================
+// //Spotify This Song ========================================
 
 var song;
-
-
 function getSong() {
     var spotify = new Spotify({
         id:keys.spotify.id,
         secret:keys.spotify.secret
     });
+
+    //Grab user input
     song = process.argv[3];
+    for (var i = 4; i < process.argv.length; i++) {
+        song += (" " + process.argv[i]);
+    }
+    
+    //If user doesnt enter song, return error
     if(!song){
         console.log(chalk.red("ERROR: You did not provide a song. How about: The Sign by Ace of Base"));
         song = "the sign ace of base"
+    } else {
+        song = song.trim();
     }
+
+    //Search spotify for song
     spotify.search({ type: 'track', query: song, limit: 2 }, function (error, data) {
         if (error) {
             return console.log(chalk.red("ERROR: " + error));
@@ -121,7 +154,7 @@ function getSong() {
 
 }
 
-//Do What It Says ===========================================
+// //Do What It Says ===========================================
 function doWhatItSays() {
 
 
@@ -130,19 +163,21 @@ function doWhatItSays() {
             return console.log(error);
         }
         else {
-            var array = data.split(",");
+            var array = data.split(',');
+            console.log(array);
+            console.log(array[1]);
             switch(array[0]){
                 case "movie-this":
-                    movieTitle = array[1].trim();
+                    movie = array[1].trim();
                     getMovie();
                     break;
                 case "concert-this":
                     artist = array[1].trim();
-                    getArtist();
+                    concertThis();
                     break;
                 case "spotify-this-song":
-                    console.log("random.txt")
-                    song = array[1].trim();
+                    song = array[1].trim();     //not actually entering something in argv[3] so just returning error?
+                    console.log(song);
                     getSong();
                     break;
                 
